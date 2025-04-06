fe4ee4a87ec3593b252acda098aa566cdd0ddd8d
Langou <113284964+LangouMeltySnow@users.noreply.github.com>
2025-04-06 17:07:14 +0800

    通用文件头

--- 文件头/FZAversion.cpp
+++ 文件头/FZAversion.cpp
@@ -2,8 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back 
-#define eb emplace_back 
+#define pb push_back
+#define eb emplace_back
 #define fi first
 #define se second
 #define ne " -> "
@@ -15,34 +15,38 @@ using namespace std;
 #define endl '\n'
 
 typedef long long ll;
-typedef pair<long long,long long> PLL;
-typedef tuple<ll,ll,ll> TLLL;
+typedef pair<long long, long long> PLL;
+typedef tuple<ll, ll, ll> TLLL;
 //const ll inf =  0x3f3f3f3f;
-const ll INF = (ll)2e18+9;
+const ll INF = (ll)2e18 + 9;
 const ll MOD = 1000000007;
 //const ll MOD = 998244353;
-
-//io functions
-inline void rd(ll &x){x=0;short f=1;char c=getchar();while((c<'0'||c>'9')&&c!='-') c=getchar();if(c=='-') f=-1,c=getchar();while(c>='0'&&c<='9') x=x*10+c-'0',c=getchar();x*=f;}  
-inline ll read(){ll x=0;short f=1;char c=getchar();while((c<'0'||c>'9')&&c!='-') c=getchar();if(c=='-') f=-1,c=getchar();while(c>='0'&&c<='9') x=x*10+c-'0',c=getchar();x*=f;return x;}  
-inline void pt(ll x){if(x<0) putchar('-'),x=-x;if(x>9) pt(x/10);putchar(x%10+'0');}
-inline void print(ll x){pt(x), puts("");}
-inline void printPLL(PLL x){pt(x.fi), putchar(' '), pt(x.se), putchar('\n');}
-inline void printVec(vector<ll> &vec){for(const auto t:vec)pt(t),putchar(' ');puts("");}
-inline void printMap(const map<ll, ll>& g) {for(const auto& [key, value]:g){cout<<"key: "<<key<<ne<<value<<" ";}puts("");}
-inline void printVPLL(vector<PLL> &vec){puts(sep);for(const auto v:vec){printPLL(v);}puts(sep);}
-inline void printVecMap(const map<ll, vector<ll>>& g) {for (const auto& [key, value] : g) { cout << "key: " << key << ne;for (const auto& v : value) {cout << v << " ";}cout << endl;}}
+inline void rd(ll &x) {x = 0; short f = 1; char ch = getchar(); while (!isdigit(ch)) {if (ch == '-') f = -1; ch = getchar();} while (isdigit(ch)) {x = (x << 3) + (x << 1) + (ch ^ 0x30); ch = getchar();} x *= f;}
+inline ll read() {ll x = 0; short f = 1; char ch = getchar(); while (!isdigit(ch)) {if (ch == '-') f = -1; ch = getchar();} while (isdigit(ch)) {x = (x << 3) + (x << 1) + (ch ^ 0x30); ch = getchar();} x *= f; return x}
+inline void pt(ll x) {if (x < 0) putchar('-'), x = -x; if (x > 9) pt(x / 10); putchar((x % 10) | 0x30);}
+inline void print(ll x) {pt(x), puts("");}
+inline void printPLL(PLL x) {pt(x.fi), putchar(' '), pt(x.se), putchar('\n');}
+inline void printVec(vector<ll> &vec) {for (const auto t : vec)pt(t), putchar(' '); puts("");}
+inline void printMap(const map<ll, ll>& g) {for (const auto& [key, value] : g) {cout << "key: " << key << ne << value << " ";} puts("");}
+inline void printVPLL(vector<PLL> &vec) {puts(sep); for (const auto v : vec) {printPLL(v);} puts(sep);}
+inline void printVecMap(const map<ll, vector<ll>>& g) {for (const auto& [key, value] : g) { cout << "key: " << key << ne; for (const auto& v : value) {cout << v << " ";} cout << endl;}}
 
 //fast pow
-ll ksm(ll a, ll b){ll res = 1;while(b){if(b&1){res=(res*a)%MOD;}a=(a*a)%MOD;b>>=1;}return res;}
+ll ksm(ll a, ll b) {ll res = 1; while (b) {if (b & 1) {res = (res * a) % MOD;} a = (a * a) % MOD; b >>= 1;} return res;}
 
-void solve(){
+void solve() {
+
 }
 
-int main(){
+int main() {
+    //ios::sync_with_stdio(0);
+    //cin.tie(0), cout.tie(0);
+
     ll t = 1;
-    //t = read();
-    while(t--){
+    rd(t);
+
+    while (t--) {
         solve();
     }
+    return 0;
 }
